2652f0183bf436dcbfa64c7fe83b3e58be744b92
Layton Webber <layton@omicsautomation.com>
2022-07-27 08:36:41 -0700

    Warnings about missing samples/genes.
    Signed-off-by: Layton Webber <layton@omicsautomation.com>

--- src/main/cpp/loader/omicsds_loader.cc
+++ src/main/cpp/loader/omicsds_loader.cc
@@ -26,6 +26,7 @@
 
 #include "omicsds_loader.h"
 #include "omicsds_export.h"
+#include "spdlog/spdlog.h"
 
 std::vector<std::string> split(std::string str, std::string sep) {
   std::vector<std::string> retval;
@@ -631,7 +632,19 @@ std::vector<OmicsCell> MatrixReader::get_next_cells() {
   uint64_t row_idx;
 
   while(parse_next(sample_name, gene_name, score)) {
-    if(!m_sample_map->count(sample_name) || !m_gene_id_map->count(gene_name)) { continue; }
+    bool skip_entry = false;
+    if(m_sample_map->count(sample_name) == 0) {
+      spdlog::warn("Sample {} not found in sample map.", sample_name);
+      skip_entry = true;
+    }
+    if(m_gene_id_map->count(gene_name) == 0) {
+      spdlog::warn("Gene {} not found in gene map.", gene_name);
+      skip_entry = true;
+    }
+    if(skip_entry) {
+      spdlog::warn("Skipping entry at Sample: {}, Gene: {}.", sample_name, gene_name);
+      continue; 
+    }
     
     row_idx = (*m_sample_map)[sample_name];
     GeneIdMap::Gene& gene = (*m_gene_id_map)[gene_name];
